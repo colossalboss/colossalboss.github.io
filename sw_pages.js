ac1ef8878f42f234c397be8967fc55d252f77df9
Ogbara G. Godstar <colossalboss@users.noreply.github.com>
2019-11-03 08:06:30 +0100

    Add service worker

---
+++ sw_pages.js
@@ -0,0 +1,50 @@
+const cacheName = 'v3';
+
+const cacheAssets = [
+  '/',
+  'index.html',
+  'styles.css',
+  'app.js',
+  'designs.js',
+  './js/sw.js'
+]
+
+self.addEventListener('install', (e) => {
+  console.log('service worker installed');
+
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log('service worker: caching files');
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', (e) => {
+  console.log('service worker activated');
+  //remove unwanted caches
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            console.log('service worker: clearing old cache');
+            return caches.delete(cache);
+          }
+        })
+      )
+    })
+  )
+})
+
+// call fetch
+self.addEventListener('fetch', (e => {
+  console.log('Service worker: fetching');
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  )
+}))
+
